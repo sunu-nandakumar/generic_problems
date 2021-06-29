1542e747901b9119297ccb4e2ee0ee9054cd2a63
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-06-29 09:37:18 +0530

    adding the float part

--- Maximum.java
+++ Maximum.java
@@ -12,10 +12,23 @@ public class Maximum {
 			}
 			return max; // returns maximum number
 		}
+		
+		public static float checkMaximum(Float d, Float e, Float f) {
+			Float max = d;
+			if(e.compareTo(max) > 0) {
+				max = e; // e is largest now
+			}
+			if(f.compareTo(max) > 0) {
+				max = f; // f is largest
+			}
+			return max; // // returns maximum number
+		}
 
 		public static void main(String[] args) {
 			int value = checkMaximum(10,23,15);
+			float result = checkMaximum(12.5f, 13.4f, 22.0f);
 			System.out.println(value);
+			System.out.println(result);
 		}
 
 
