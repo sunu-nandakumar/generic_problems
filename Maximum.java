68cf453024231071e9ef11d2b43293c58c167d35
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-06-29 09:50:15 +0530

    adding the fifth uc

--- Maximum.java
+++ Maximum.java
@@ -1,42 +1,52 @@
 package com.generic;
 
-public class Maximum {
-	//calculating Maximum of Three Numbers using CompareTo method
-	// Claculating Maximum of 3 Integers, Floating point, Strings
-		public static <T extends Comparable<T>> T checkMaximum(T x, T y, T z) {
-			T max = x;
-			if(y.compareTo(max) > 0) {
-				max = y; // y is maximum now
-			}
-			if(z.compareTo(max) > 0) {
-				max = z; // z is maximum now
-			}
-			return max; // returns maximum value
+public class Maximum<T extends Comparable<T>> {
+	
+	T x, y, z;
+	
+	public Maximum(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	
+	//determine the largest of three Comparable Objects
+	public static <T extends Comparable<T>> T checkMaximum(T x, T y, T z) {
+		T max = x;
+		if(y.compareTo(max) > 0) {
+			max = y; // y is largest thus for
 		}
-		
-		public static <T extends Comparable<T>> T checkMaximum_With_MoreThan_ThreeParameters(T... elements) {
-			T max = elements[0];
-			for(T maximumParametereter : elements) {
-				if(maximumParametereter.compareTo(max) > 0) {
-					max = maximumParametereter;
-				}
-			}
-			return max;
+		if(z.compareTo(max) > 0) {
+			max = z; // z is largest now
 		}
+		printMax(max);
+		return max; // returns maximum
+	}
 
-		public static void main(String[] args) {
-			Integer value = checkMaximum(10,23,15);
-			Double result = checkMaximum(12.5, 13.4, 22.0);
-			String fruitMax = checkMaximum("Apple", "Peach", "Banana");
-			Integer valueForThanThree = checkMaximum_With_MoreThan_ThreeParameters(10,23,15,20,24);
-			Double resultForThanThree = checkMaximum_With_MoreThan_ThreeParameters(12.5, 13.4, 22.0,24.0,20.8);
-			String fruitForThanThree = checkMaximum_With_MoreThan_ThreeParameters("Apple", "Peach", "Banana", "Mango", "Orange"); 
-			System.out.println(value);
-			System.out.println(result);
-			System.out.println(fruitMax);
-			System.out.println(valueForThanThree);
-			System.out.println(resultForThanThree);
-			System.out.println(fruitForThanThree);
+	public static String testMaximum(String x, String y, String z) {
+		String max = x;
+		if(y.compareTo(max) > 0) {
+			max = y; // y is the largest so far
 		}
+		if(z.compareTo(max) > 0) {
+			max = z; // z is the largest now
+		}
+		printMax( max);
+		return max; // returns the largest so for
+	}
+	
+	public static <T> void printMax(T max) {
+		System.out.println("Max of Three  is" + max);
+	}
+
+	public static void main(String[] args) {
+		Integer result = checkMaximum(10,23,15);
+		Double value = checkMaximum(12.5, 13.4, 22.0);
+		String a = "Apple", b = "peach", c = "Banana"; 
+		Maximum.testMaximum(a, b, c);
+		printMax(result);
+		printMax(value);
+	}
 
 }
